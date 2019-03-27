5b3cc7a6c324a2c9be5d2897feee5098c5aca9e7
Eduardo Pilla Portilho <eduardo@portilho.com>
2019-03-27 19:50:30 -0300

    Calculator 0.1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,130 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+
+const numbers = [1,2,3,4,5,6,7,8,9,0]
+const ops = {
+  '+': (a, b) => a+b,
+  '-': (a, b) => a-b,
+  '*': (a, b) => a*b,
+  '/': (a, b) => a/b
+}
+
+const Calculator = styled.div`
+  flex-direction: column;
+  display: flex;
+  margin: auto;
+  width: 260px;
+  background: black;
+  border-radius: 15px;
+  padding: 5px;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+`
+const Display = styled.div`
+  color: white;
+  text-align: right;
+  padding: 40px 10px 5px;
+  font-size: 40px;
+`
+const Keypad = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: start;
+`
+
+const Numbers = styled.div`
+  display: flex;
+  width: 200px;
+  flex-wrap: wrap;
+  justify-content: center;
+`
+
+const Number = styled.div`
+  padding: 10px 16px;
+  background: darkslategray;
+  color: white;
+  border-radius: 50%;
+  margin: 10px;
+  font-size: 20px;
+  text-align: center;
+
+  min-width: 10px;
+  :nth-child(10) {
+    padding-right: 85px;
+    border-radius: 21.5px;
+  }
+`
+
+const Comma = styled(Number)``
+
+const Operations = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const Operation = styled(Number)`
+  background: orange;
+`
 
 class App extends Component {
+  state = {
+    display: '0',
+    op: '',
+    buffer: ''
+  }
+
+  handleNum = (number) => {
+    this.setState(({display}) => {
+      const prev = display === '0' ? '' : display
+      return {
+        display: prev + number
+      }
+    })
+  }
+
+  handleOp = (op) => {
+    this.setState(({display}) => {
+      return {
+        op,
+        buffer: display,
+        display: ''
+      }
+    })
+  }
+
+  handleEq = () => {
+    const { display, op, buffer } = this.state
+    if (display === '' || op === '' || buffer === '') return
+    this.setState(({display, op, buffer}) => {
+      const result = ops[op](parseInt(buffer), parseInt(display))
+      return {
+        op: '',
+        buffer: '',
+        display: result
+      }
+    })
+  }
+
   render() {
+    const { display, op, buffer } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Calculator>
+        <Display>{ buffer } { op } { display }</Display>
+        <Keypad>
+          <Numbers>
+            {numbers.map(number => (
+              <Number key={number} onClick={() => this.handleNum(number)}>{number}</Number>
+            ))}
+            <Comma>,</Comma>
+          </Numbers>
+
+          <Operations>
+            {Object.keys(ops).map(op => (
+              <Operation key={op} onClick={() => this.handleOp(op)}>{op}</Operation>
+            ))}
+            <Operation onClick={this.handleEq}>=</Operation>
+          </Operations>
+        </Keypad>
+      </Calculator>
     );
   }
 }
